03a7433554bb5d60adae922d3b8d2668b692ab91
Natouche68 <ngrossheny@gmail.com>
2024-02-04 17:15:40 +0100

    Add a color palette

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,16 @@
 		src: url("Unbounded.ttf") format("truetype");
 	}
 
+	:root {
+		--bg-color: #1e1e1f;
+		--text-color: #d3e9f5;
+		--accent-color: #63b0cd;
+	}
+
 	:global(body) {
 		font-family: "Unbounded", sans-serif;
+		font-size: 16px;
+		background-color: var(--bg-color);
+		color: var(--text-color);
 	}
 </style>
