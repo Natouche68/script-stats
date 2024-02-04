45f1f7602b560dae6af86a3c379b2ac98ef1d747
Natouche68 <ngrossheny@gmail.com>
2024-02-04 17:06:30 +0100

    Add the Unbounded font

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<title>Script Stats</title>
+</svelte:head>
+
+<slot />
+
+<style>
+	@font-face {
+		font-family: "Unbounded";
+		src: url("Unbounded.ttf") format("truetype");
+	}
+
+	:global(body) {
+		font-family: "Unbounded", sans-serif;
+	}
+</style>
